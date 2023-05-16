bc5b4d0578e157499e719a98ec149014cfc9c89f
Ben Rosenblum <rosenblumb@gmail.com>
2023-05-16 12:09:08 -0500

    Fixes DecimalType
    Signed-off-by: Ben Rosenblum <rosenblumb@gmail.com>

--- bundles/org.openhab.binding.rachio/src/main/java/org/openhab/binding/rachio/internal/handler/RachioZoneHandler.java
+++ bundles/org.openhab.binding.rachio/src/main/java/org/openhab/binding/rachio/internal/handler/RachioZoneHandler.java
@@ -15,6 +15,7 @@ package org.openhab.binding.rachio.internal.handler;
 import static org.openhab.binding.rachio.internal.RachioBindingConstants.*;
 import static org.openhab.binding.rachio.internal.RachioUtils.getTimestamp;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -245,11 +246,11 @@ public class RachioZoneHandler extends BaseThingHandler implements RachioStatusL
         RachioZone z = zone;
         if (z != null) {
             updateChannel(CHANNEL_ZONE_NAME, new StringType(z.name));
-            updateChannel(CHANNEL_ZONE_NUMBER, new DecimalType(z.zoneNumber));
+            updateChannel(CHANNEL_ZONE_NUMBER, new DecimalType(new BigDecimal(z.zoneNumber)));
             updateChannel(CHANNEL_ZONE_ENABLED, z.getEnabled());
             updateChannel(CHANNEL_ZONE_RUN, OnOffType.OFF);
-            updateChannel(CHANNEL_ZONE_RUN_TIME, new DecimalType(z.getStartRunTime()));
-            updateChannel(CHANNEL_ZONE_RUN_TOTAL, new DecimalType(z.runtime));
+            updateChannel(CHANNEL_ZONE_RUN_TIME, new DecimalType(new BigDecimal(z.getStartRunTime())));
+            updateChannel(CHANNEL_ZONE_RUN_TOTAL, new DecimalType(new BigDecimal(z.runtime)));
             updateChannel(CHANNEL_ZONE_IMAGEURL, new StringType(z.imageUrl));
             updateChannel(CHANNEL_LAST_EVENT, new StringType(z.getEvent()));
             DateTimeType ts = z.getEventTime();
